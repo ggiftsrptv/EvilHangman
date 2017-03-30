d014d57f4797807984d401a1dcc52653dbb899da
Dezio <jackzahajung@gmail.com>
2017-03-30 16:28:50 +0700

    add file java

---
+++ EvilHangman.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evilhangman;
+
+import java.util.ArrayList;
+
+
+public class EvilHangman {
+    public int wordLength;
+    public int totalGuessRemaining;
+    public int correctGuess;
+    public int incorrect;
+    public boolean foundLetter;
+    public ArrayList<String> wordFamilies = new ArrayList();
+    public ArrayList<String> correctGuesses = new ArrayList(); //can't create the same name like int.
+    public ArrayList<String> allGuess = new ArrayList();
+
+    public EvilHangman() {
+        
+    }
+    public void lengthOfWord() {
+        
+    }
+    public void setWordFamilies() {
+        
+    }
+    public int decreaseGuess(int x) {
+        return x;
+    }
+    public boolean compareLetter(boolean x) {
+        return x;
+    }
+    public int increaseIncorrectGuess(int x) {
+        return x;
+    }
+    public void fillPositionLetter() {
+        
+    }
+    public int checkNumberBlankPosition(int x) {
+        return x;
+    }
+    public void updateScreen(){
+        
+    }
+    
+}
