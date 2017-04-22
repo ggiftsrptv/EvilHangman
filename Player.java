328912cfee8ee7a0bb4eef32302951d5b001d0ed
pongngai <pongzaeiei@hotmail.com>
2017-04-22 13:10:19 +0700

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,22 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package evilhangman;
-
-
-public class Player {
-   public char letterGuessed;
-   public char playAgain;
+import java.util.ArrayList;
+public class Player extends EvilHangman{
+ public char letterGussed;
+ public char playAgain;
+ public void setLength(){
    
-   public void setLength(){
-       
-   }
-   public char guessLetter(char x) {
-       return x;
-   }
-   public char againToPlay(char x) {
-       return x;
-   }
+ }
+ public char guessLetter(){
+  return letterGussed; 
+ }
+ public char againToplay(){
+   return playAgain;
+ }
 }
