5a7fbbbe132cbe6219ce05c37adc3fb7b5527fb2
pongngai <pongzaeiei@hotmail.com>
2017-04-24 11:27:32 +0700

    read text

--- EvilHangman.java
+++ EvilHangman.java
@@ -1,38 +1,62 @@
+import java.util.*;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EvilHangman{
- public int wordLength;
- public int totalGuessRemaining;
- public int correctGuess;
- public int incorrect;
- public boolean foundLetter;
- public ArrayList<String>wordFamilies;
- public ArrayList<String>correctGuess1;
- public ArrayList<String>allGuess;
- public EvilHangman(){
-   
- }
- public void lengthOfWord(){
-   
- }
- public void setWordFamilies(){
-   
- }
- public int decreaseGuess(){
-   return totalGuessRemaining;
- }
- public boolean compareLetter(){
-   return foundLetter;
- }
- public int increaseIncorrectGuess(){
-  return incorrect; 
- }
- public void fillPositionLetter(){
-   
- }
- public int checkNumberBlankPosition(){
-   return wordLength;
- }
- public void updateScreen(){
-   
- }
+  public int wordLength;
+  public int totalGuessRemaining;
+  public int correctGuess;
+  public int incorrect;
+  public boolean foundLetter;
+  public String[]wordFamilies = new String[235000];
+  public ArrayList<String>correctGuess1;
+  public ArrayList<String>allGuess;
+  
+  public EvilHangman(){
+  
+  }
+  public void lengthOfWord(int length,int guess){
+    wordLength = length;
+    totalGuessRemaining = guess;
+  }
+  public void setWordFamilies(){
+    Scanner s = null;
+    try{
+      s = new Scanner(new File("C:\\Users\\USER\\Desktop\\dictionary.txt")); 
+    }catch(Exception e ){
+      throw new RuntimeException(e);
+    }
+    int i = 0;
+    while(s.hasNext()){//keep word dictionary into temp
+     String temp = s.nextLine();
+     if(temp.length() == wordLength){
+      wordFamilies[i] = temp;
+      i++;
+     }
+    }
+  }
+  public int decreaseGuess(){
+    return totalGuessRemaining;
+  }
+  public boolean compareLetter(){
+    return foundLetter;
+  }
+  public int increaseIncorrectGuess(){
+    return incorrect; 
+  }
+  public void fillPositionLetter(){
+    
+  }
+  public int checkNumberBlankPosition(){
+    return wordLength;
+  }
+  public void updateScreen(){
+    
+  }
+  public void print(){
+    for(String a : wordFamilies){
+      System.out.println(a); 
+    }
+  }
 }
